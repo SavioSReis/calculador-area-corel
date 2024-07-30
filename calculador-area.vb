3f750f5c43b125d75383b97e0f93df9ee7bdae4e
SavioSReis <hjd44.sa@gmail.com>
2024-07-29 21:24:51 -0300

    Deixando variáveis mais descritivas

--- calculador-area.vb
+++ calculador-area.vb
@@ -1,6 +1,6 @@
 Sub CalculateContourArea()
-    Dim s As Shape
-    Dim dArea As Double
+    Dim formato As Shape
+    Dim area As Double
 
     ' Verifica se há uma seleção
     If ActiveSelection.Shapes.Count = 0 Then
@@ -9,17 +9,17 @@ Sub CalculateContourArea()
     End If
 
     ' Obtém a forma selecionada
-    Set s = ActiveSelection.Shapes(1)
+    Set formato = ActiveSelection.Shapes(1)
 
     ' Verifica se a forma é um contorno
-    If Not s.Type = cdrCurveShape Then
+    If Not formato.Type = cdrCurveShape Then
         MsgBox "A forma selecionada não é um contorno."
         Exit Sub
     End If
 
     ' Calcula a área do contorno
-    dArea = s.Curve.Area
+    area = Round(formato.Curve.Area, 5)
 
     ' Também exibe uma mensagem com a área
-    MsgBox "Área do contorno: " & dArea & " unidades quadradas"
+    MsgBox "Área do contorno: " & area & " unidades quadradas"
 End Sub
